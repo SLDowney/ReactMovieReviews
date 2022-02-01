f0228d73df93a67db173a9e88f9e9345a5818383
SLDowney <sdowney@outlook.com>
2022-02-01 17:53:54 -0330

    mongodb added to app

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -4,6 +4,7 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import { MongoClient } from 'mongodb';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -24,9 +25,25 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
 
 app.get('/hello', (req, res) => { res.send("Hello")});
+
 app.post('/hello', (req, res) => { res.send(`Hello there ${req.body.name}`)})
-app.get('/api/movies', (req, res) => {
-    res.send(movieData);
+
+app.get('/api/movies', async (req, res) => {
+
+    try {
+        const client = await MongoClient.connect('mongodb://localhost:27017', {useNewUrlParser: true});
+        const db = client.db('my-movies');
+
+        const movieInfo = await db.collection('movies').find({}).toArray();
+        console.log(movieInfo);
+        res.status(200).json(movieInfo);
+        client.close();
+    }
+    catch (error) {
+        res.status(500).json( { message: "Error connecting to db ", error});
+    }    
+    
+    // res.send(movieData);
 })
 
 //app.get('*', (req, res) => { res.sendFile(path.join(__dirname + '/build/index.html'))})
